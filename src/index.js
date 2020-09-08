61c1edaab228943c7316317a0c4b76e8845e652e
Gabriel Sugai <gabrielsugai@gmail.com>
2020-09-08 01:55:58 -0300

    Cria um componente unico para a pagina

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-import Primeiro from './components/basics/First'
-import Parametros from './components/basics/Parameters'
-import Fragment from './components/basics/Fragment'
+import App from './App'
 
-ReactDOM.render(<div> 
-                    <Primeiro></Primeiro>
-                    <Parametros titulo='React app' subtitulo='Projeto de testes'/>
-
-                    <Fragment />
-                </div>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
