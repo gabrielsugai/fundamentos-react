2356cbc85037d96d32e49444927c282bcdbf3a2f
Gabriel Sugai <gabrielsugai@gmail.com>
2020-09-08 01:39:52 -0300

    Cria componente que recebe parametros

---
+++ src/components/basics/Parameters.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function Parameters(props) {
+    return(
+        <div>
+            <h2>{props.titulo}</h2>
+            <h3>{props.subtitulo}</h3>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basics/First'
+import Parametros from './components/basics/Parameters'
 
 ReactDOM.render(<div> 
                     <Primeiro></Primeiro>
+                    <Parametros titulo='React app' subtitulo='Projeto de testes'/>
                 </div>, document.getElementById('root'))
