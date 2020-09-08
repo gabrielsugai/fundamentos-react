329c3eb135ebe96474449ceb157945cade9a558a
Gabriel Sugai <gabrielsugai@gmail.com>
2020-09-08 01:12:07 -0300

    Adiciona JSX ao componente

--- src/components/basics/First.js
+++ src/components/basics/First.js
@@ -1,3 +1,8 @@
+import React from 'react'
 export default function First(){
-    return 'Primeiro Componente'
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+        </div>
+    )
 }
