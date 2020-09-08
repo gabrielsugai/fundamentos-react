9daad40932d69eee732dc22ece9f196d8f7aedaf
Gabriel Sugai <gabrielsugai@gmail.com>
2020-09-08 01:48:45 -0300

    Adiciona exemplo de fragmento

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import React from 'react'
 
 import Primeiro from './components/basics/First'
 import Parametros from './components/basics/Parameters'
+import Fragment from './components/basics/Fragment'
 
 ReactDOM.render(<div> 
                     <Primeiro></Primeiro>
                     <Parametros titulo='React app' subtitulo='Projeto de testes'/>
+
+                    <Fragment />
                 </div>, document.getElementById('root'))
